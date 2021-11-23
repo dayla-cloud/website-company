6787ebf25a0ca33d703141b2faf23ffe2ebd3395
fin-dianekim <64294351+challanstudio@users.noreply.github.com>
2021-11-23 16:19:04 +0900

    toggle appealpoint list!!!

--- productdetail.js
+++ productdetail.js
@@ -1,1 +1,12 @@
-localStorage;
+const btn = document.querySelectorAll("#description-appealpoint");
+const text = document.querySelectorAll(".description_appealpoint_details");
+const toggle = document.getElementById("description-appeallist-toggle");
+
+function active() {
+  btn.forEach(el => el.classList.toggle("height"));
+  text.forEach(p => p.classList.toggle("display"));
+}
+
+// btn.forEach(e => e.addEventListener("click", active));
+
+toggle.addEventListener("click", active);
