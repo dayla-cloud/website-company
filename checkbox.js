cabd58bdaa3fe703ee377b093b720bad8dd62bd6
fin-dianekim <64294351+challanstudio@users.noreply.github.com>
2021-12-07 18:30:01 +0900

    211207 / button / text / swiper

--- checkbox.js
+++ checkbox.js
@@ -1,10 +1,12 @@
 const chCnt = document.getElementById("checkbox-contact");
 const togglePg = document.getElementById("toggle-page");
 
-chCnt.addEventListener("change", function () {
+chCnt.addEventListener("change", function (e) {
   if (this.checked) {
     togglePg.classList.add("open-page");
   } else {
     togglePg.classList.remove("open-page");
   }
 });
+
+// 오픈페이지가 없을 때 페이지영역을 클릭하면 알럿을 켜주세요
