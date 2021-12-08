223577d16cbd04fe81f81f94c48637b4da552efa
fin-dianekim <64294351+challanstudio@users.noreply.github.com>
2021-12-08 14:31:02 +0900

    211208 / change color of 同意します / edit error of navlist

--- checkbox.js
+++ checkbox.js
@@ -9,4 +9,16 @@ chCnt.addEventListener("change", function (e) {
   }
 });
 
-// 오픈페이지가 없을 때 페이지영역을 클릭하면 알럿을 켜주세요
+// pointer-events: none;
+
+// const chCnt = document.getElementById("checkbox-contact");
+// const togglePg = document.getElementById("toggle-page");
+
+// chCnt.addEventListener("change", function (e) {
+//   if (this.checked) {
+//     togglePg.classList.add("open-page");
+//   } else {
+//     togglePg.classList.remove("open-page");
+
+//   }
+// });
